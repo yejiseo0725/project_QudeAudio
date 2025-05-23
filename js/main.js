0ae5bb0aca0db9a989862cb1150a1b062be82de1
keroppi <yejiseo0725@gmail.com>
2025-05-23 16:41:48 +0900

    gsap_logoWrap

--- js/main.js
+++ js/main.js
@@ -6,4 +6,50 @@ window.onload = function () {
   menuOpen.addEventListener("click", () => {
     menuBox.classList.toggle("on");
   });
+
+  // GSAP
+  gsap.registerPlugin(ScrollTrigger);
+
+  // visual
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".visual",
+        start: "100% 100%",
+        end: "100% 0%",
+        scrub: 1,
+        markers: true,
+      },
+    })
+
+    .to(
+      ".logoWrap #j",
+      { x: -150, y: 250, rotate: 20, ease: "none", duration: 5 },
+      0
+    )
+    .to(
+      ".logoWrap #y",
+      { x: -30, y: 150, rotate: -10, ease: "none", duration: 5 },
+      0
+    )
+    .to(
+      ".logoWrap #o",
+      { x: 0, y: 400, rotate: -10, ease: "none", duration: 5 },
+      0
+    )
+    .to(
+      ".logoWrap #u",
+      { x: 50, y: 300, rotate: 10, ease: "none", duration: 5 },
+      0
+    )
+    .to(
+      ".logoWrap #n",
+      { x: 100, y: 100, rotate: -10, ease: "none", duration: 5 },
+      0
+    )
+    .to(
+      ".logoWrap #g",
+      { x: 50, y: 450, rotate: 20, ease: "none", duration: 5 },
+      0
+    );
 };
